d431ca3fa9d4816fcc5de5524b42f1aced034679
Ian Montes <ianpdmontes@gmail.com>
2024-09-07 16:30:09 +0100

    SQUID: updating README, creating jest.config.ts, installing husky & lint-staged

---
+++ jest.config.ts
@@ -0,0 +1,25 @@
+export default {
+    moduleFileExtensions: ["js", "json", "ts"],
+    rootDir: "src",
+    testRegex: ".*\\.spec\\.ts$",
+    transform: {
+      "^.+\\.(t|j)s$": "ts-jest",
+    },
+    collectCoverageFrom: ["**/*.(t|j)s"],
+    moduleNameMapper: {
+      "^src/(.*)$": "<rootDir>/$1",
+    },
+    collectCoverage: true,
+    coverageDirectory: "../coverage",
+    testEnvironment: "node",
+    coverageReporters: ["json", "html"],
+    coverageThreshold: {
+      global: {
+        branches: 85,
+        functions: 85,
+        lines: 85,
+        statements: 85,
+      },
+    },
+  };
+  
